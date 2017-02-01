2ddb3f16f62ac0dd8500a35402a3c0dce06aaa52
psi974 <psi974479@gmail.com>
2017-02-01 15:24:23 +0400

    Création des entitées et de la BDD

--- src/MdL/BilletterieBundle/Controller/SelectBilletController.php
+++ src/MdL/BilletterieBundle/Controller/SelectBilletController.php
@@ -4,17 +4,17 @@
 
 namespace MdL\BilletterieBundle\Controller;
 
+use Eniams\TicketBundle\Form\TicketType;
 use MdL\BilletterieBundle\Entity\SelectBillet;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class SelectBilletController extends Controller
 {
 	public function indexAction()
     {
-       
         return $this->render('MdLBilletterieBundle:SelectBillet:index.html.twig');
     }
-
 }
