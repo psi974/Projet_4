8100557126de080e97f2c7fa63ba82925ab4e04c
psi974 <psi974479@gmail.com>
2017-02-07 16:37:52 +0400

    Evolution Formulaire -> Mise en forme et tests - Formatage des données -> MAJ des attr et options diverses - Consolidation des entités et FormType

--- src/MdL/BilletterieBundle/Controller/SelectBilletController.php
+++ src/MdL/BilletterieBundle/Controller/SelectBilletController.php
@@ -13,30 +13,35 @@ class SelectBilletController extends Controller
 {
     public function indexAction(Request $request)
     {
+
         // Formulaire
         $commande = new Commande();
         $form = $this->get('form.factory')->create(CommandeType::class, $commande);
 
 
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
-            $em = $this->getDoctrine()->getManager();
+        if ($request->isMethod('POST'))
+        {
 
             // Pour test
+            $dateResa = $_POST['mdl_billetteriebundle_commande']['dtVisite'];
+            $commande->setDtVisite($dateResa);
 
-            var_dump($commande);die();
+            var_dump($dateResa);
+            die();
 
+            $em = $this->getDoctrine()->getManager();
             $em->persist($commande);
             $em->flush();
 
+
             $request->getSession()->getFlashBag()->add('notice', 'Commande bien enregistrée.');
 
-            return $this->render('MdLBilletterieBundle:SelectBillet:index.html.twig', array(
-                'form' => $form->createView(),
-            ));
+            return $this->redirectToRoute('mdl_billetterie_view');
         }
+
         // Formulaire invalide
         return $this->render('MdLBilletterieBundle:SelectBillet:index.html.twig', array(
-            'form' => $form->createView(),
+            'form' => $form->createView()
         ));
     }
 }
